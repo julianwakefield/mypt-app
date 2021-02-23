9723f9357125154a326cb73d3468d647f23ee0a8
Julian Wakefield <julianwakefield@outlook.com>
2021-02-23 14:50:18 +0000

    index

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Exercise.create (name: "Squat", body_parts: "legs, calves")
-Exercise.create (name: "Press up", body_parts: "chest, arms")
-Exercise.create (name: "Sit up", body_parts: "abs")
-Exercise.create (name: "Military press", body_parts: "shoulders")
-Exercise.create (name: "Pull up", body_parts: "back")
+Exercise.create(name: "Squat", body_parts: "legs, calves")
+Exercise.create(name: "Press up", body_parts: "chest, arms")
+Exercise.create(name: "Sit up", body_parts: "abs")
+Exercise.create(name: "Military press", body_parts: "shoulders")
+Exercise.create(name: "Pull up", body_parts: "back")
