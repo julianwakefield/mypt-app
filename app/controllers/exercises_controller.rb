e2045d3cd0e9c252b68d8a27a16fef32f2715c10
Julian Wakefield <julianwakefield@outlook.com>
2021-02-23 14:36:02 +0000

    exercise controller done

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,24 @@
 class ExercisesController < ApplicationController
+
+    def index
+        @exercises = Exercise.all
+    end
+    
+    def new
+        @exercise = Exercise.new 
+    end
+
+
+    def create
+        @exercise = Exercise.new(exercise_params)
+        if exercise.save!
+            redirect_to exercise_path(@exercise)
+        else
+        render.new
+        end
+    end
+
+    
+
+
 end
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,16 @@
 class WorkoutsController < ApplicationController
+
+    def new
+        @workout = Workout.new 
+    end
+
+    def create
+        # @workout = Workout.new(workout_params)
+        # if workout.save!
+        #     redirect_to workout_path(@workout)
+        # else
+        # render.new
+        # end
+    end
+
 end
